50908834648ae237962baabcaabf49706f86fbc7
Nguyen Thanh Luan <luann4099@gmail.com>
2022-09-26 11:22:40 +0700

    reconfig mysql

--- ktl/php/mysql.php
+++ ktl/php/mysql.php
@@ -1,10 +1,9 @@
 <?php
-	$con = mysqli_connect("localhost","root","","ktl",3308);
-	if(!$con)
-	{
-		echo "error";
-	}
+$con = mysqli_connect("127.0.0.1", "root", "koodinh@", "ktl", 3306);
+if (!$con) {
+    echo "error";
+}
 //	@mysqli_select_db("todito", $con) or die("DB 연결실패");
-	mysqli_query($con,"set names utf8");
+mysqli_query($con, "set names utf8");
 
 ?>
