eea547f54d91edf5b5bb3c7f8616ee8a04c5c794
mien <2502mien@gmail.com>
2022-09-28 17:41:24 +0700

    add

---
+++ ktl/infor_general.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<?php include 'php/common_header.php' ?>
+<link rel="stylesheet" href="css/login.css">
+<body>
+<div class="wrap">
+    <?php include 'php/common_header_menu.php' ?>
+
+    <div class="container-fluid" style="margin-top: 2rem">
+        <div class="text">
+            직무관리를 위한 채용정보
+        </div>
+        <div class="container-fluid" style="border: 1px solid #dee2e6">
+            <div style="padding: 0.5rem">
+                <div>
+                    Nguyễn cao Miên 1991/ Nam
+                </div>
+                <div class="row" >
+                    <div class="mx">
+                        <i class="fa-solid fa-square-envelope" style="color: grey"></i> <span>hihi@gmail.com</span>
+                    </div>
+                    <div class="mx">
+                        <i class="fa-solid fa-phone" style="color: grey"></i> <span>0987678888</span>
+                    </div>
+                    <div class="mx">
+                        <i class="fa-solid fa-mobile-screen-button" style="color: grey"></i> <span>0987679999</span>
+                    </div>
+                    <div class="mx">
+                        <i class="fa-solid fa-house-user" style="color: grey"></i><span>Address: 123/123/123</span>
+                    </div>
+                </div>
+
+
+                <div class="container-fluid">
+                    <div class="row ">
+                        <div class="col" style="margin-top: 2rem" >
+
+                            <table class="table table-striped" style="font-size: 16px">
+                                <thead>
+                                <tr>
+                                    <th scope="col">lorem</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <th scope="row">lorem</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <div class="col">
+
+                            <div class="col">
+                                <div class="text ">
+                                    학업 능력
+                                </div>
+                                <table class="table table-striped" style="font-size: 16px">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">lorem</th>
+                                        <th scope="col">First</th>
+                                        <th scope="col">Last</th>
+                                        <th scope="col">Handle</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">lorem</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+                    <div class="row ">
+                        <div class="col" >
+                            <div class="text ">
+                                학업 능력
+                            </div>
+                            <table class="table table-striped" style="font-size: 16px">
+                                <thead>
+                                <tr>
+                                    <th scope="col">lorem</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <th scope="row">lorem</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="col">
+                            <div class="col" >
+                                <div class="text ">
+                                    학업 능력
+                                </div>
+                                <table class="table table-striped" style="font-size: 16px">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">lorem</th>
+                                        <th scope="col">First</th>
+                                        <th scope="col">Last</th>
+                                        <th scope="col">Handle</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">lorem</th>
+                                        <td>Mark</td>
+                                        <td>ôtto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+                    <div class="row ">
+                        <div class="col" >
+                            <div class="text ">
+                                학업 능력
+                            </div>
+                            <table class="table table-striped" style="font-size: 16px">
+                                <thead>
+                                <tr>
+                                    <th scope="col">lorem</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <th scope="row">lorem</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="col">
+                            <div class="col" >
+                                <div class="text ">
+                                    학업 능력
+                                </div>
+                                <table class="table table-striped" style="font-size: 16px">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">lorem</th>
+                                        <th scope="col">First</th>
+                                        <th scope="col">Last</th>
+                                        <th scope="col">Handle</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <th scope="row">lorem</th>
+                                        <td>Mark</td>
+                                        <td>ôtto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <!--    --><?php //include 'php/common_footer.php' ?>
+</div>
+<?php include 'php/common_script.php' ?>
+</body>
+<style>
+    .mx {
+        margin-right: 0.5rem;
+        margin-left: 0.5rem;
+    }
+
+    .col {
+        padding-left: unset !important;
+    }
+
+    .text {
+        font-size: 1.3rem;
+        color: #212121;
+        font-weight: bold;
+    }
+    .table-striped tbody tr:nth-of-type(odd) {
+        background: none !important;
+    }
+    .table thead th {
+        border-bottom: 1px solid #dee2e6;
+    }
+    .table {
+        border: 1px solid #dee2e6;
+    }
+</style>
+</html>
