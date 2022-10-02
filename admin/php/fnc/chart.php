5fe6f0013df28bf0fe5a95ff8610c4d737227933
quangdz <coolquanghuu@gmail.com>
2022-10-02 17:00:38 +0700

    hot fix

--- admin/php/fnc/chart.php
+++ admin/php/fnc/chart.php
@@ -4,27 +4,32 @@ function connect(){
     include "../mysql.php";
     include "../crypt.php";
     // chưa học hết cấp 3
-    $a1="select count(*) as total from apply_step_2 a where a.not_graduated = 0 group by not_graduated ";
+    $a1="select count(*) as total from apply_step_2 a where a.not_graduated = 0 group by not_graduated";
     $resultA1= mysqli_query($con, $a1);
     // tốt nghiệp cap 3
-    $a2="select count(*) from apply_step_2 a  where a.status_graduation_high_school = 0 and a.not_graduated = 1  group by status_graduation_high_school";
-    $resultA2= mysqli_query($con, $a2);
-    // tôt nghiệp cao đẳng học
-    $a3="select count(*) from apply_step_2 a where a.status_graduation_high_school = 1 and status_graduate_university = 0 and date_graduate_postgraduate = 0 group by status_graduation_high_school ";
+
+    $a3="select count(*) as total from apply_step_2 a where a.status_graduation_high_school = 1 and a.only_highSchool = 1  group by status_graduation_high_school";
     $resultA3= mysqli_query($con, $a3);
-    // tốt nghiệp đại học
-    $a4="select count(*) from apply_step_2 a  where a.status_graduate_university = 1 and date_graduate_postgraduate = 0 group by status_graduate_university";
+    // tôt nghiệp cao đẳng học
+    $a4="select distinct count(*) as total from university a where a.type_school = 'college' and status_graduate = 1 group by a.type_school";
     $resultA4= mysqli_query($con, $a4);
-    // tốt nghiệp cao học
-    $a5="select count(*) from apply_step_2 a  where a.date_graduate_postgraduate = 1 group by date_graduate_postgraduate";
+    // tốt nghiệp đại học
+    $a5="select distinct count(*) as total from university a where a.type_school = 'university'  and status_graduate = 1  group by a.type_school";
     $resultA5= mysqli_query($con, $a5);
+    // tốt nghiệp cao học
+    $a6="select distinct count(*) as total from university a where a.type_school = 'postgraduate' and status_graduate = 1 and degree = 0 group by a.type_school";
+    $resultA6= mysqli_query($con, $a6);
+    $a7="select distinct count(*) as total from university a where a.type_school = 'postgraduate' and degree = 1 group by a.type_school";
+    $resultA7= mysqli_query($con, $a7);
+
     $data1= mysqli_fetch_assoc($resultA1);
-    $data2= mysqli_fetch_assoc($resultA2);
     $data3= mysqli_fetch_assoc($resultA3);
     $data4= mysqli_fetch_assoc($resultA4);
     $data5= mysqli_fetch_assoc($resultA5);
+    $data6= mysqli_fetch_assoc($resultA6);
+    $data7= mysqli_fetch_assoc($resultA7);
 
-    $result = [$data1 == null ? "0" : "1",$data2 == null ? "0" : "1",$data3== null ? "0" : "1",$data4== null ? "0" : "1",$data5== null ? "0" : "1"];
+    $result = [$data1 == null ? "0" : $data1['total'],$data3== null ? "0" : $data3['total'],$data4== null ? "0" : $data4['total'],$data5== null ? "0" : $data5['total'],$data6== null ? "0" : $data6['total'],$data7== null ? "0" : $data7['total']];
    echo json_encode($result);
    mysqli_close($con);
 }
