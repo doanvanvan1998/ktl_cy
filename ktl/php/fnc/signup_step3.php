de7981b279b73db6aeb263791271c5b38de3b74d
vandv <vanbkat14121102@gmail.com>
2022-10-03 13:28:08 -0700

    add ableid step3_4

--- ktl/php/fnc/signup_step3.php
+++ ktl/php/fnc/signup_step3.php
@@ -1,25 +1,7 @@
 <?php
 require '../mysql.php';
-//$name_equals = $_POST['name_equals'];
-//$issued_by = $_POST['issued_by'];
-//$time_equals = $_POST['time_equals'];
-//$contest_name = $_POST['contest_name'];
-//$granting_agencies = $_POST['granting_agencies'];
-//$time_start = $_POST['time_start'];
-//$time_end = $_POST['time_end'];
-//$content_prize = $_POST['content_prize'];
-//$link = $_POST['link'];
-//
-//
-
-//
-//$query = "INSERT INTO `apply_step_3`(`file_portlio`, `userlink`, `name_equals`, `issued_by`, `time_equals`, `contest_name`, `granting_agencies`, `time_start`, `time_end`, `content_prize`, `able_id`)VALUES ('$filenamenew','$link','$name_equals','$issued_by','$time_equals','$contest_name','$granting_agencies','$time_start','$time_end','$content_prize','1')";
-//$result = mysqli_query($con, $query);
-//if (!$result) {
-//    echo "Error: " . $query . "<br>" . mysqli_error($con);
-//} else {
-//    echo "Success";
-//}
+session_start();
+$id = $_SESSION["Id"];
 $data = json_decode($_POST['data']);
 $file = isset($_FILES['portfolio']) ? $_FILES['portfolio'] : null;
 $filenamenew = '';
@@ -54,7 +36,7 @@ if ($file != null) {
 
 // handle insert db apply_step_3
 $link = $data->{'link'};
-$insertStep3Query = "INSERT INTO apply_step_3 (`id`, `userlink`, `file_portlio`, `able_id`) VALUES (NULL, '$link', '$filenamenew', '1')";
+$insertStep3Query = "INSERT INTO apply_step_3 (`id`, `userlink`, `file_portlio`, `able_id`) VALUES (NULL, '$link', '$filenamenew', $id)";
 $result = mysqli_query($con, $insertStep3Query);
 
 if (!$result) {
--- ktl/php/fnc/signup_step_4.php
+++ ktl/php/fnc/signup_step_4.php
@@ -1,8 +1,9 @@
 <?php
 require '../mysql.php';
 
-
+session_start();
 $data = json_decode($_POST['data']);
+$id = $_SESSION["Id"];
 
 
 foreach ($data as $record) {
@@ -10,7 +11,7 @@ foreach ($data as $record) {
     $selfIntroductionType = $record->{'selfIntroductionType'};
     $selfIntroductionContent = $record->{'selfIntroductionContent'};
 
-    $insertQuery = "INSERT INTO `main_resume` (`id`, `content`, `able_id`, `id_type`, `name_type`) VALUES (NULL, '$selfIntroductionContent', '1', '$selfIntroductionType', '$selfIntroductionTypeName');";
+    $insertQuery = "INSERT INTO `main_resume` (`id`, `content`, `able_id`, `id_type`, `name_type`) VALUES (NULL, '$selfIntroductionContent', $id, '$selfIntroductionType', '$selfIntroductionTypeName');";
     $result = mysqli_query($con, $insertQuery);
     if (!$result) {
         echo false;
