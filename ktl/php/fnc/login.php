9d66b2e010f89fa85e4ca5605b420f2d35414df4
mien <2502mien@gmail.com>
2022-10-04 01:05:23 +0700

    add

--- ktl/php/fnc/login.php
+++ ktl/php/fnc/login.php
@@ -1,18 +1,23 @@
 <?php
+session_start();
 require "../mysql.php";
 require "../crypt.php";
 
 $email = Encrypt($_POST['email'], $secret_key, $secret_iv);
 $pass = Encrypt($_POST['pass'], $secret_key, $secret_iv);
 $name = $_POST['name'];
-$query = "SELECT * FROM `recruit_able_user` WHERE email='$email' AND pass='$pass' AND username='$name'";
+
+$query = "SELECT * FROM recruit_able_user WHERE email='$email' AND pass='$pass' AND username='$name' AND active=1";
+echo $query;
 $result = mysqli_query($con, $query);
 $row = mysqli_fetch_array($result);
-if ($row) {
+if ($row == 0) {
+    echo 'please check your email or password';
+
+//    header("Location: ../index.php");
+} else {
     $_SESSION['email'] = $email;
     $_SESSION['pass'] = $pass;
-    header("Location: ../index.php");
-} else {
-    echo 'please check your email or password';
+    echo "success";
 }
 ?>
