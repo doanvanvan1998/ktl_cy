76be202be1ed77d18e09ea30cb38b11d532d819f
vandv <vanbkat14121102@gmail.com>
2022-10-04 14:02:17 -0700

    clean code

--- ktl/php/fnc/example_send.php
+++ ktl/php/fnc/example_send.php
@@ -1,53 +1,42 @@
 <?php
 session_start();
-function Encrypt($str, $secret_key='secret key', $secret_iv='secret iv')
-{
-		$key = hash('sha256', $secret_key);
-		$iv = substr(hash('sha256', $secret_iv), 0, 32)    ;
-
-		return str_replace("=", "", base64_encode(
-								 openssl_encrypt($str, "AES-256-CBC", $key, 0, $iv))
-		);
-}
-
-
-function Decrypt($str, $secret_key='secret key', $secret_iv='secret iv')
-{
-		$key = hash('sha256', $secret_key);
-		$iv = substr(hash('sha256', $secret_iv), 0, 32);
-
-		return openssl_decrypt(
-						base64_decode($str), "AES-256-CBC", $key, 0, $iv
-		);
-}
+ob_start();
 
 include_once "../coolsms.php";
-
 /*
  **  api_key and api_secret can be obtained from www.coolsms.co.kr/credentials
  */
 
-$p 										= $_POST["phone"];
-$phonechk 						= $_POST["phonechk"];
 
-$secret_iv = "arti@$!develop";
-$secret_key = "86730";
+include "mysql.php";
+include "crypt.php";
 
-$p = Decrypt($p, $secret_key, $secret_iv);
-// initiate rest api sdk object
-$rest = new coolsms("NCSQOI484NRYYAZB", "NZCQZ4WVOBUNJT1ZKVV6FTXS6EZEASRS");
+$Id 								= $_POST["Id"];
+$query="select id,username,phone,email,pass,acept_rule,status_pass,rand_code from recruit_able_user where id='$Id'";
 
+$TransResult = mysqli_query($con,$query);
+$TransRow = mysqli_fetch_array($TransResult,MYSQLI_NUM);
+$phone =Decrypt($TransRow['phone'], $secret_key, $secret_iv);
+$rand_code = $TransRow['rand_code'];
+$email = $TransRow['email'];
+
+$rest = new coolsms("NCSXOU8UCIPG01NE", "XCLICLKYKXBPCW0CXVXTYLE2S97OM7OX");
 /*
  **  5 options(timestamp, to, from, type, text) are mandatory. must be filled
  */
-$chkNum = rand ( 1000,9999 );
+$chkNum = rand(1000, 9999);
 $options = new StdClass();
 $options->timestamp = (string)time();
-$options->to = $p;
-$options->from = '0317550765';
-$options->text = "GMC certification number [".$chkNum."] 입니다.";
-$options->app_version = 'test app 1.2';  // application name and version
+$options->to = $phone;
+$options->from = '02-2662-5571';
+
+//$encrypted = Encrypt($p, $secret_key, $secret_iv);
+
+$textq = $rand_code;
 $options->type = 'SMS'; // SMS, MMS, LMS, ATA
+
+$options->text = $textq;
+$options->app_version = 'test app 1.2';  // application name and version
 
 // Alimtalk example. https://www.coolsms.co.kr/AboutAlimTalk
 // $options->type = 'ATA';
@@ -57,7 +46,7 @@ $options->type = 'SMS'; // SMS, MMS, LMS, ATA
 //  Optional parameters for your own needs
 // $options->image = 'test.png'; 			// image for MMS. type must be set as 'MMS'
 // $options->refname = '';					// Reference name
-$options->country = $phonechk;
+$options->country = 82;
 
 
 // $options->datetime = '20140106153000';	// Format must be(YYYYMMDDHHMISS) 2014 01 06 15 30 00 (2014 Jan 06th 3pm 30 00)
@@ -70,15 +59,13 @@ $options->country = $phonechk;
  * added REST API v1.5
  **/
 // $options->os_platform = 'Windows 7';		// Operating System. SDK creates automatically if empty
- $options->dev_lang = 'PHP 5.3.3';		// Application development language. SDK creates automatically if empty
+$options->dev_lang = 'PHP 5.3.3';        // Application development language. SDK creates automatically if empty
 // $options->sdk_version = 'PHP SDK 1.1';	// SDK version being used. SDK creates automatically if empty
 
 // send messages
 $response = $rest->send($options);
-
 // get result
 $result = $response->getResult();
-
-echo $chkNum;
+echo "success";
 
 ?>
