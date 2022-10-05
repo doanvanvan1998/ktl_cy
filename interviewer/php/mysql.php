2b928a902d7c8379fbec1f1d86d307c8c4a7862e
quangdz <coolquanghuu@gmail.com>
2022-10-05 14:47:34 +0700

    hohotfix

--- interviewer/php/mysql.php
+++ interviewer/php/mysql.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "ktl",3308);
+$con = mysqli_connect("ableup.cafe24.com", "ableup", "ab1004!@", "ktl",22);
 
 //	@mysqli_select_db("todito", $con) or die("DB 연결실패");
 
